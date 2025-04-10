084bc6b7f69a23fe49407c10e11b476847283fc5
vlademirodc <149390803+vlademirodc@users.noreply.github.com>
2025-04-10 14:43:12 -0300

    Update generate.sh

--- monitor/generate.sh
+++ monitor/generate.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+echo "Baixando a lista de servidores"
+hbmclip https://raw.githubusercontent.com/vlademirodc/hbmclip/refs/heads/main/monitor/download_csv.prg
 echo "Importando os dados de planilha.csv"
 hbmclip https://raw.githubusercontent.com/vlademirodc/hbmclip/refs/heads/main/monitor/imp_planilha.prg
 echo "Gerando o nmap"
